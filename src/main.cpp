43bea0527f44d4d193aa99080109a6a30bd1586f
Wadoyoka <atsushi021028@gmail.com>
2025-01-20 22:40:51 +0900

    Soracomと共有できた

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <string>
 #include "esp_camera.h"
 #include <WiFi.h>
 #include <Firebase_ESP_Client.h>
@@ -8,13 +10,30 @@
 #include <HTTPClient.h>
 #include <base64.h>
 #include <time.h>
+#include <PubSubClient.h>
+#include <WireGuard-ESP32.h>
 
 // Select camera model
 #define CAMERA_MODEL_M5STACK_PSRAM // M5Stack with PSRAM
 #include "camera_pins.h"
 
-const char *ssid = "*******";     // Replace with your Wi-Fi SSID
-const char *password = "*******"; // Replace with your Wi-Fi password
+const char *ssid = "WIFI";
+const char *password = "WIFI";
+const char *mqtt_server = "YOURSERVER";
+const int mqtt_port = 99999;
+const char *mqtt_topic = "YOURTOPIC";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+// SORACOM ArcのWireGuard情報
+const char *private_key = "YOUR_SORACOM_ARC_PRIVATE_KEY";
+IPAddress local_ip(10, 0, 0, 1); // SORACOM ArcのIPアドレス (例)
+const char *peer_public_key = "YOUR_SORACOM_ARC_PUBLIC_KEY";
+const char *endpoint_address = "YOUR_SORACOM_ARC_ENDPOINT.arc.soracom.io";
+const int endpoint_port = 11010; // WireGuardのデフォルトポート
+
+WireGuard wg;
 
 // Firebase project credentials
 #define FIREBASE_PROJECT_ID "secret"              // プロジェクトID
@@ -39,74 +58,44 @@ void syncTime()
     Serial.println("Time synchronized.");
 }
 
-bool getLatestData()
+void setup_wifi()
 {
-    if (Firebase.ready())
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED)
     {
-        String documentPath = "slackMessage";
-
-        Serial.print("Querying Firestore for latest data...");
-
-        if (Firebase.Firestore.getDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str()))
-        {
-            Serial.println("Query successful!");
-            // Serial.printf("ok\n%s\n\n", fbdo.payload().c_str());
-            // Firebase JSON からドキュメントIDを取得
-            DynamicJsonDocument doc(2048); // 必要に応じてサイズを調整
-            deserializeJson(doc, fbdo.payload().c_str());
-
-            // document.name フィールドの確認
-            if (!doc["documents"].isNull() && doc["documents"].size() > 0)
-            {
-                String documentStatus = doc["documents"][0]["fields"]["status"]["booleanValue"].as<String>();
-                Serial.println("JsonParse successful!");
-                return documentStatus.equals("true");
-            }
-            else
-            {
-                Serial.println("No documents found or documents array is empty!");
-                return false;
-            }
-        }
-        else
-        {
-            Serial.println("Query failed: ");
-            Serial.println(fbdo.errorReason());
-            return false;
-        }
+        delay(500);
+        Serial.print(".");
     }
-    else
-    {
-        Serial.println("Firebase is not ready");
-        return false;
-    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    // Google Public DNSを設定
+    IPAddress dns(8, 8, 8, 8);
+    WiFi.config(WiFi.localIP(), WiFi.gatewayIP(), WiFi.subnetMask(), dns);
+    // DNSが利用可能になるまで待機
+    delay(1000);
 }
 
-void updateStatus()
+void connectToWireGuard()
 {
-    if (Firebase.ready())
-    {
-        String collection = "slackMessage";
-        String documentPath = "/slackStatus";
-        documentPath = collection + documentPath;
-        FirebaseJson content;
+    Serial.println("Connecting to SORACOM Arc...");
 
-        content.set("fields/status/booleanValue", false);
+    Serial.print("Adjusting system time: ");
+    configTime(9 * 60 * 60, 0, "ntp.jst.mfeed.ad.jp", "ntp.nict.jp", "time.google.com");
+    delay(3000);
+    Serial.println("done.");
 
-        // まずは更新を試みる
-        if (Firebase.Firestore.patchDocument(&fbdo, FIREBASE_PROJECT_ID, "", documentPath.c_str(), content.raw(), ""))
-        {
-            Serial.println("Data updated in Firestore successfully!");
-        }
-        else
-        {
-            Serial.println("Data updated in Firestore Failed");
-        }
-    }
-    else
-    {
-        Serial.println("Firebase is not ready");
-    }
+    Serial.print("Connect to SORACOM Arc (WireGuard):");
+    wg.begin(local_ip, private_key, endpoint_address, peer_public_key, endpoint_port);
+    Serial.println("done.");
 }
 
 bool uploadImageToFirebase(camera_fb_t *fb)
@@ -130,6 +119,79 @@ bool uploadImageToFirebase(camera_fb_t *fb)
     {
         Serial.println("Image upload failed");
         return false;
+    }
+}
+
+bool reconnectWiFi()
+{
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        return true; // Wi-Fiが接続済みの場合、何もしない
+    }
+
+    Serial.println("WiFi disconnected. Attempting to reconnect...");
+
+    WiFi.disconnect();
+    WiFi.begin(ssid, password);
+
+    unsigned long startAttemptTime = millis();
+    const unsigned long timeout = 10000; // 再接続タイムアウト (10秒)
+
+    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < timeout)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        Serial.println("\nWiFi reconnected successfully");
+        return true;
+    }
+    else
+    {
+        Serial.println("\nWiFi reconnection failed. Retrying in 5 seconds...");
+        return false;
+    }
+}
+
+void take_photo()
+{
+    camera_fb_t *fb = NULL;
+    fb = esp_camera_fb_get();
+    esp_camera_fb_return(fb);
+    fb = NULL;
+    fb =esp_camera_fb_get();
+
+    if (!fb)
+    {
+        Serial.println("Camera capture failed");
+        ESP.restart();
+        return;
+    }
+    // Wi-Fiの接続状態をチェックし、再接続を試みる
+    if (reconnectWiFi())
+    {
+        Serial.println("============Update Mode=========");
+        if (uploadImageToFirebase(fb))
+        {
+            delay(2000);
+        }
+        delay(2000);
+    }
+    esp_camera_fb_return(fb);
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+    StaticJsonDocument<200> slackMessage; // 必要に応じてサイズを調整
+    deserializeJson(slackMessage, payload);
+    const char *message = slackMessage["message"];
+    Serial.println(message == "photo");
+
+    if (strcmp(message, "photo") == 0)
+    {
+        take_photo();
     }
 }
 
@@ -199,13 +261,7 @@ void setup()
     }
 
     // Connect to Wi-Fi
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("\nWiFi connected");
+    setup_wifi();
 
     // Initialize Firebase
     firebaseConfig.api_key = API_KEY;
@@ -217,72 +273,38 @@ void setup()
     Firebase.reconnectWiFi(true);
     syncTime();
     Serial.println("Firebase initialized");
+
+    connectToWireGuard();
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(callback);
 }
 
-bool reconnectWiFi()
+void reconnect()
 {
-    if (WiFi.status() == WL_CONNECTED)
+    while (!client.connected())
     {
-        return true; // Wi-Fiが接続済みの場合、何もしない
-    }
-
-    Serial.println("WiFi disconnected. Attempting to reconnect...");
-
-    WiFi.disconnect();
-    WiFi.begin(ssid, password);
-
-    unsigned long startAttemptTime = millis();
-    const unsigned long timeout = 10000; // 再接続タイムアウト (10秒)
-
-    while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < timeout)
-    {
-        delay(500);
-        Serial.print(".");
-    }
-
-    if (WiFi.status() == WL_CONNECTED)
-    {
-        Serial.println("\nWiFi reconnected successfully");
-        return true;
-    }
-    else
-    {
-        Serial.println("\nWiFi reconnection failed. Retrying in 5 seconds...");
-        return false;
+        Serial.print("Attempting MQTT connection...");
+        if (client.connect("M5StackClient"))
+        {
+            Serial.println("connected");
+            client.subscribe(mqtt_topic);
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            delay(5000);
+        }
     }
 }
 
 void loop()
 {
-    delay(5000);
-    camera_fb_t *fb = NULL;
-    fb = esp_camera_fb_get();
-    if (!fb)
-    {
-        Serial.println("Camera capture failed");
-        esp_camera_fb_return(fb);
-        return;
-    }
-    // Wi-Fiの接続状態をチェックし、再接続を試みる
-    if (reconnectWiFi())
-    {
-        if (!fb)
-        {
-            Serial.println("Camera capture failed");
-            return;
-        }
 
-        if (getLatestData())
-        {
-            Serial.println("============Update Mode=========");
-
-            if (uploadImageToFirebase(fb))
-            {
-                delay(2000);
-                updateStatus();
-            }
-            delay(2000);
-        }
+    if (!client.connected())
+    {
+        reconnect();
     }
-    esp_camera_fb_return(fb);
+    client.loop();
 }
